727a872c914345605d751491652ba2eb007df043
GersonTimbe <gersontimbe76@gmail.com>
2025-07-14 17:58:43 +0200

    Update 10_main2_simulation2.py

--- 10_main2_simulation2.py
+++ 10_main2_simulation2.py
@@ -1,8 +1,8 @@
 """
 @author: Gerson Joao Timbe
 
-Script to simulate the combustion Process in a combustion chamber of a jet engine (assumptions a made) 
-The main goal is to ilustrate the temperature and air-fuel injection and consumption variations
+Script to simulate the combustion Process in a combustion chamber of a jet engine (Some assumptions were made). 
+The main goal is to ilustrate temperature and air-fuel injection and consumption variations inside the chamber.
 """
 
 import numpy as np
